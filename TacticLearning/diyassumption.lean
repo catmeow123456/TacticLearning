6c1da82b0ab0ad6346b82c2512055920d99b3309
catmeow123456 <eden@stu.pku.edu.cn>
2024-10-17 20:01:09 +0800

    diyassumption

---
+++ TacticLearning/diyassumption.lean
@@ -0,0 +1,27 @@
+import Lean
+import Mathlib
+
+open Lean Meta Elab Tactic
+
+def throwErr (tacticName : Name) (mvarId : MVarId)
+    (msg? : Option MessageData := none) : MetaM α :=
+  match msg? with
+  | none => throwError "tactic '{tacticName}' failed\n{mvarId}"
+  | some msg => throwError "tactic '{tacticName}' failed, {msg}\n{mvarId}"
+
+def Lean.MVarId.assumptCore (mvarId : MVarId) : MetaM Bool :=
+  mvarId.withContext do
+    mvarId.checkNotAssigned `assumption
+    match (← findLocalDeclWithType? (← mvarId.getType)) with
+    | none => return false
+    | some fvarId => mvarId.assign (mkFVar fvarId); return true
+
+def Lean.MVarId.assumpt (mvarId : MVarId) : MetaM Unit :=
+  unless (← mvarId.assumptCore) do
+    throwErr `assumption mvarId
+
+def evalAssumption : Tactic := fun _ =>
+  liftMetaTactic fun mvarId => do mvarId.assumpt; pure []
+
+theorem test (h : 1 ≤ 2) : 1 ≤ 2 := by
+  assumption
