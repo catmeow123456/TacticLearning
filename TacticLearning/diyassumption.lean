80cd5e6a4a447370501879330a469849ce631d2d
catmeow123456 <eden@stu.pku.edu.cn>
2024-10-18 19:39:23 +0800

    update info

--- TacticLearning/diyassumption.lean
+++ TacticLearning/diyassumption.lean
@@ -4,18 +4,30 @@ import Mathlib
 open Lean Meta Elab Tactic
 
 
-/-
-throwErr 扔出报错信息，包括 tactic 名称，元变量 id，以及可选的报错信息
+/-!
+
+# diyassumption
+
+在这个文件中，我们定义了一个 tactic `diyAssumption`，
+它检查 LocalContext 中是否有一个类型与当前待赋值目标的类型相同的 LocalDecl，
+如果有，则将这个 LocalDecl 的 fvarid 赋值给当前目标。
+
+## 主要定义
+
+* `throwErr` : throwErr 扔出报错信息，包括 tactic 名称，元变量 id，以及可选的报错信息
+
+* `assumptCore` : 用于检查在 LocalContext 中是否有一个类型与当前 MVar 的类型相同的 LocalDecl，
+
+* `assumpt` : 包装了 assumptCore，如果 assumptCore 返回 false，则抛出一个错误
+
 -/
+
 def throwErr {α: Type} (tacticName : Name) (mvarId : MVarId)
     (msg? : Option MessageData := none) : MetaM α :=
   match msg? with
   | none => throwError "tactic '{tacticName}' failed\n{mvarId}"
   | some msg => throwError "tactic '{tacticName}' failed, {msg}\n{mvarId}"
 
-/-
-assumptCore 用于检查在 LocalContext 中是否有一个类型与当前 MVar 的类型相同的 LocalDecl，
--/
 def Lean.MVarId.assumptCore (mvarId : MVarId) : MetaM Bool :=
   mvarId.withContext do
     mvarId.checkNotAssigned `assumption
@@ -23,9 +35,6 @@ def Lean.MVarId.assumptCore (mvarId : MVarId) : MetaM Bool :=
     | none => return false
     | some fvarId => mvarId.assign (mkFVar fvarId); return true
 
-/-
-assumpt 包装了 assumptCore，如果 assumptCore 返回 false，则抛出一个错误
--/
 def Lean.MVarId.assumpt (mvarId : MVarId) : MetaM Unit :=
   unless (← mvarId.assumptCore) do
     throwErr `assumption mvarId
