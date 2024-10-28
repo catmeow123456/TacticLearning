972fe286b99833de2b562fb1aced5e770998af9f
eden@stu.pku.edu.cn <eden@stu.pku.edu.cn>
2024-10-28 15:27:52 +0800

    update showtype

--- TacticLearning/display/showdecl.lean
+++ TacticLearning/display/showdecl.lean
@@ -23,20 +23,23 @@ elab "#expr" "[" t:term "]" : command =>
 #check CoreM -- ReaderT Context $ StateRefT State (EIO Exception)
 
 #check isDefEq
-/-- Return a local declaration whose type is definitionally equal to `type`. -/
+/--
+Return a list of local declarations
+whose type is definitionally equal to `type`.
+-/
 def ListLocalDeclWithType? (type : Expr) : MetaM (Array FVarId) := do
   let list_change: MetaM $ Array FVarId :=
     (← getLCtx).foldl (init := do pure #[]) fun lst decl =>
       if decl.isImplementationDetail then
         lst
       else do
+        let s ← saveState
         if (← isDefEq type decl.type) then
+          s.restore
           return (← lst).push decl.fvarId
         else
           lst
   return (← list_change)
-
-#check TSyntax `Term
 
 elab "elabterm" t:term : tactic => do
   let t ← Term.elabTerm t none
@@ -48,25 +51,30 @@ elab "elabterm" t:term : tactic => do
 elab "showtype" t:term : tactic => do
   let t ← Term.elabTerm t none
   liftMetaTactic fun mvarId => do
-    let result ← ListLocalDeclWithType? t
+    let result: Array FVarId ← ListLocalDeclWithType? t
     match result with
     | #[] => throwError "No local declaration with type {t}"
     | _ =>
-      let msg: MessageData := ← result.foldl (init := do pure "")
+      let msg: MessageData ← result.foldl (init := do pure "")
         fun s fvarId => do
-          let fvar := mkFVar fvarId
+          let fvar: Expr := .fvar fvarId
           pure ((← s) ++ " " ++ m!"{fvar}")
       logInfo (m!"Local declarations with type {t}:\n" ++ msg)
       return [mvarId]
 
-example (h h2: 1 = 2) (w₁ w₂ : Nat) : 1 = 2 ∧ 2 = 1:= by
-  constructor
+-- meta variable   MVarId    ? : SomeType
+-- free variable   FVarId    h : SomeType
+-- use mkFVar/mkMVar or Expr.fvar/.mvar to transform a FVarId/MVarId into an Expr
+
+example (h h2: 1 = 2) (w₁ w₂ : Nat) (hw: w₁ = w₂)
+  (h₁ : ∀ i : Nat, i < i + 1) (h₂ : ∀ j₁ j₂ : List Nat, j₁ ++ j₂ = j₂ ++ j₁)
+    : False := by
   elabterm (1:Nat)
   elabterm (w₁)
   elabterm (h)
-  showtype (1 = 2)
+  elabterm (1 :: [2])
+  showtype (1 = _)
   showtype (Nat)
-  · sorry
-  sorry
-
-#check OfNat.ofNat
+  showtype _ = _
+  showtype ∀ _:_, _
+  stop {}
